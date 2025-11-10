06f517e7d460f36cf7197eb3fe61126febdb8a67
Bismark Kwaku Kete <kbismarkkwaku@gmail.com>
2025-11-10 23:21:09 +0000

    Fixed the bug in guessing_game.py and created a new program, sum_game to sum up all numbers in a list.

--- P-Learning/guessing_game.py
+++ P-Learning/guessing_game.py
@@ -5,12 +5,11 @@ import random
 
 secret_number = random.randint(1,10)
 
-user_number = int(input("""I'm thinking of a number between 1 and 10. Can you" 
-                            guess it? """))
+user_number = int(input("I'm thinking of a number between 1 and 10. Can you guess it? "))
 
 #print(secret_number)
 match user_number:
-    case secret_number:
+    case _ if secret_number == user_number:
         print("Congratulations, you guessed it!")
     case _:
         if user_number > secret_number:
---
+++ P-Learning/sum_game.py
@@ -0,0 +1,10 @@
+# A python program to sum up all the numbers in a list using the for loop
+
+
+numbers = [1, 5, 3, 9]
+total = 0
+
+for number in numbers:
+    total += number
+
+print(total)
